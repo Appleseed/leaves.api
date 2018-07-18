65859343ca28409e0c707c3c5125a7a2fbaaf36e
Ankit Gupta <31555452+AnkitGupta2020@users.noreply.github.com>
2018-07-18 10:14:38 -0400

    Takes URL Parameter/prints out in html

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,16 +1,12 @@
-
 //Load express module with `require` directive
 var express = require('express')
 var app = express()
 
 //Define request response in root URL (/)
-var url = "http://www.example.com/article.html";
-function functUrl(newurl) {
-    return newurl;              // The function returns the product of p1 and p2
-}
 
 app.get('/', function(req, res){
-  res.send('url: ' + functUrl(url)); //prints url parameter
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  res.send('url: ' + fullUrl); //prints url parameter
 });
 
 //Launch listening server on port 8081
