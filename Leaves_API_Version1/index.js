aac8a72bacba1d66229e2a8875dc977650fa340d
Jagannath Bilgi <jagannath.bilgi@anant.us>
2018-07-28 02:00:01 +0530

    added todo sample code

--- Leaves_API_Version1/index.js
+++ Leaves_API_Version1/index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var bodyParser = require("body-parser");
 var app = express()
 
 var request = require("request");
@@ -10,6 +11,12 @@ app.get('/', function(req, res){ //returns URL
   fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   res.send('ANANT CORPORATION LEAVES API- url: ' + fullUrl + ", ALL METADATA IN CONSOLE.LOG WHEN Index.js is run."); //prints url parameter
 });
+
+var routes = require("./routes.js");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+routes(app);
 
 //Comment the following ONCE fullUrl is not an empty page
 fullUrl = 'http://www.example.com/article.html';
@@ -51,31 +58,31 @@ extractor.analyze().then(function(text) {
 console.log("IMAGE");
 
 var wkhtmltoimage = require('wkhtmltoimage');
-wkhtmltoimage.generate(fullUrl, { output: 'imageout.png' });
+wkhtmltoimage.generate(fullUrl, { output: './images/imageout.png' });
 console.log("Image generated in folder: imageout.png")
 console.log('*****************************************************************')
 
 const fs = require('fs');
 const resizeImg = require('resize-img');
 
-resizeImg(fs.readFileSync('imageout.png'), {width: 2048, height: 898}).then(buf => {
-    fs.writeFileSync('imageout-BIG.png', buf);
+resizeImg(fs.readFileSync('./images/imageout.png'), {width: 2048, height: 898}).then(buf => {
+    fs.writeFileSync('./images/imageout-BIG.png', buf);
     console.log("THUMB BIG");
     console.log("Image generated in folder: imageout-BIG.png")
     console.log('*****************************************************************')
 
 });
 
-resizeImg(fs.readFileSync('imageout.png'), {width: 1024, height: 449}).then(buf => {
-    fs.writeFileSync('imageout-MEDIUM.png', buf);
+resizeImg(fs.readFileSync('./images/imageout.png'), {width: 1024, height: 449}).then(buf => {
+    fs.writeFileSync('./images/imageout-MEDIUM.png', buf);
     console.log("THUMB MEDIUM");
     console.log("Image generated in folder: imageout-MEDIUM.png")
     console.log('*****************************************************************')
 
 });
 
-resizeImg(fs.readFileSync('imageout.png'), {width: 512, height: 224}).then(buf => {
-    fs.writeFileSync('imageout-SMALL.png', buf);
+resizeImg(fs.readFileSync('./images/imageout.png'), {width: 512, height: 224}).then(buf => {
+    fs.writeFileSync('./images/imageout-SMALL.png', buf);
     console.log("THUMB SMALL");
     console.log("Image generated in folder: imageout-SMALL.png")
     console.log('*****************************************************************')
@@ -119,6 +126,6 @@ read(fullUrl, function(err, article, meta) {
 
 //Note: Embed.ly account costs money
 
-app.listen(8081, function () {
-  //console.log('app listening on port 8081!')
+var server = app.listen(8081, function () {
+  console.log('app listening on port 8081!')
 })
