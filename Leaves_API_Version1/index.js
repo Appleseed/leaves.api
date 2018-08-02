948743f652df8c269344f7c68705cf01dc6d7ac0
Jagannath Bilgi <jagannath.bilgi@anant.us>
2018-08-02 22:49:55 +0530

    Leaves.API - Refactor

--- Leaves_API_Version1/index.js
+++ Leaves_API_Version1/index.js
@@ -14,152 +14,178 @@ app.get('/', function(req, res){ //returns URL
   res.send('ANANT CORPORATION LEAVES API- url: ' + fullUrl + ", ALL METADATA IN CONSOLE.LOG WHEN Index.js is run."); //prints url parameter
 });
 
-
 var bodytest = '';
+
+class LeavesStaticClass {
+    static leavesContent (url, title, res_type, res) {
+	  request(url, function(error, response, body) {
+		console.log(title)
+		bodytest = body;
+		console.log(body); //Prints out raw/body of the url in js
+		console.log('*****************************************************************')
+		if (res_type === 'JSON'){
+		  res.status(200).send({
+			body: bodytest,
+			})
+		} else {
+		  res.status(200).send( bodytest )
+		}
+		});
+    }
+    static leavesTextCard (url, heading, req_type, res) {
+		var thistitle = ''; //Stores title for later
+		var thisbody = ''; //Stores body for later
+
+		var extractor = new BodyExtractor({
+			url: url
+		  });
+		  var textcontent = ''
+		  extractor.analyze().then(function(text) {
+			textcontent = extractor.title;
+			textcontent += extractor.mainText;
+			if (req_type === 'TEXT') {
+				console.log(extractor.title);
+				console.log(extractor.mainText);
+				res.status(200).send({
+					textcontent})
+			} else {
+				var embedhtmlcode = '<blockquote class="embedly-card"><h4><a href="' + fullUrl +'">' + thistitle + '</a></h4><p>'+ thisbody+'</p></blockquote><script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>';
+				console.log(heading);
+				console.log(embedhtmlcode)
+				res.status(200).send({embedhtmlcode});
+			}
+		  })
+	}
+    static leavesImages (url, heading, image_path, options, res) {
+		var webshot = require('webshot');
+		console.log(heading);
+		if (options === ""){
+	    webshot(url, image_path, function(err){
+			  res.sendFile(image_path);
+		})}
+		else{webshot(url, image_path, options, function(err){
+			  res.sendFile(image_path);
+		})};
+	}
+    static leavesPageRank (url, heading, res) {
+		var pagerank = require('google-pagerank');
+		var rankerpage = 0;
+		pagerank(url, function(err, rank) {
+			console.log(heading);
+			console.log('Got pagerank', rank);
+			rankerpage = rank;
+			res.status(200).send({
+			rank: rankerpage,});
+		});
+	}
+    static leavesRead (url, heading, res) {
+		var read = require('node-readability');
+		read(url, function(error, article, meta) {
+			console.log(heading);
+			var text = article.title;
+			text += article.content;
+			text += article.body;
+			console.log(text); //Prints out raw/body of the url in js
+			console.log('*****************************************************************')
+			article.close();
+			res.status(200).send({text})
+		});
+	}
+}
+
 app.get('/content/full/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-  request(urlpath, function(error, response, body) {
-    console.log("FULL HTML")
-    bodytest = body;
-    console.log(body); //Prints out raw/body of the url in js
-    console.log('*****************************************************************')
-	  res.status(200).send({
-		body: bodytest,
-	  })
-	});
+  LeavesStaticClass.leavesContent(urlpath, "FULL HTML", 'JSON', res);
 });
 
 app.get('/content/raw/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-  request(urlpath, function(error, response, body) {
-    console.log("RAW HTML")
-    bodytest = body;
-    console.log(body); //Prints out raw/body of the url in js
-    console.log('*****************************************************************')
-	  res.status(200).send(
-		bodytest)
-	});
-});
-
-var read = require('node-readability');
-
-app.get('/content/read/:url', (req, res) => { //first
-  var urlpath = 'http://'+req.params.url;
-  read(urlpath, function(error, article, meta) {
-    console.log("READABLE HTML")
-	text = article.title;
-    text += article.content;
-	text += article.body;
-    console.log(text); //Prints out raw/body of the url in js
-    console.log('*****************************************************************')
-	article.close();
-	res.status(200).send({
-		text})
-	});
+  LeavesStaticClass.leavesContent(urlpath, "RAW HTML", 'HTML', res);
 });
 
 app.get('/content/text/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-  var extractor = new BodyExtractor({
-    url: urlpath
-  });
-  extractor.analyze().then(function(text) {
-    textcontent = extractor.title;
-    textcontent += extractor.mainText;
-	console.log(extractor.title);
-	console.log(extractor.mainText);
-	res.status(200).send({
-		textcontent})
-	});
+  LeavesStaticClass.leavesTextCard(urlpath, "TEXT", "TEXT", res);
 });
-
-var thistitle = ''; //Stores title for later
-var thisbody = ''; //Stores body for later
-
-var BodyExtractor = require('extract-main-text');
 
 app.get('/meta/card/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-  var extractor = new BodyExtractor({
-    url: urlpath
-  });
-  extractor.analyze().then(function(text) {
-    thistitle = extractor.title;
-    thisbody = extractor.mainText;
-    var embedhtmlcode = '<blockquote class="embedly-card"><h4><a href="' + fullUrl +'">' + thistitle + '</a></h4><p>'+ thisbody+'</p></blockquote><script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>';
-	console.log("EMBED CARD HTML");
-	console.log(embedhtmlcode)
-	res.status(200).send({
-		embedhtmlcode})
-	});
+  LeavesStaticClass.leavesTextCard(urlpath, "EMBED CARD HTML", "CARD", res);
 });
 
-var webshot = require('webshot');
-app.get('/images/test/:url', (req, res) => { //first
+app.get('/images/test/:url', (req, res) => {
   var urlpath = 'http://'+req.params.url;
-  webshot(urlpath, '/app/imageout.png', function(err){
-	  res.sendFile('/app/imageout.png');
-  });
+  LeavesStaticClass.leavesImages(urlpath, "Default image", '/app/images/imageout_def.png', "", res);
 });
-
-
-var wkhtmltoimage = require('wkhtmltoimage');
-const fs = require('fs');
-const resizeImg = require('resize-img');
 
 app.get('/images/first/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-//  var options = {defaultWhiteBackground: true}
-//  wkhtmltoimage.generate(urlpath, { output: '/app/images/imageout.png' });
-  webshot(urlpath, '/app/images/imageout.png', function(err){
-  console.log("Image generated in folder: imageout.png")
-  res.sendFile('/app/images/imageout.png')});
+  var options = {
+	  screenSize: {
+		width: 320
+	  , height: 480
+	  }
+	, shotSize: {
+		width: 320
+	  , height: 'all'
+	  }
+	  }
+  LeavesStaticClass.leavesImages(urlpath, "First image", '/app/images/imageout.png', options, res);
 });
 
 app.get('/images/thumb/large/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-  wkhtmltoimage.generate(urlpath, { output: '/app/images/imageout.png' });
-  resizeImg(fs.readFileSync('/app/images/imageout.png'), {width: 2048, height: 898}).then(buf => {
-    fs.writeFileSync('/app/images/imageout-BIG.png', buf);
-    console.log("THUMB BIG");
-    console.log("Image generated in folder: imageout-BIG.png")
-	res.sendFile('/app/images/imageout-BIG.png')});
+  var options = {
+  screenSize: {
+    width: 2048
+  , height: 898
+  }
+, shotSize: {
+    width: 'all'
+  , height: 'all'
+  }
+  }
+  LeavesStaticClass.leavesImages(urlpath, "THUMB BIG", '/app/images/imageout-BIG.png', options, res);
 });
 
 app.get('/images/thumb/medium/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-  wkhtmltoimage.generate(urlpath, { output: '/app/images/imageout.png' });
-  resizeImg(fs.readFileSync('/app/images/imageout.png'), {width: 1024, height: 449}).then(buf => {
-    fs.writeFileSync('/app/images/imageout-MEDIUM.png', buf);
-    console.log("THUMB MEDIUM");
-    console.log("Image generated in folder: imageout-MEDIUM.png")
-	res.sendFile('/app/images/imageout-MEDIUM.png')});
+  var options = {
+  screenSize: {
+    width: 1024
+  , height: 449
+  }
+, shotSize: {
+    width: 'all'
+  , height: 'all'
+  }
+  }
+  LeavesStaticClass.leavesImages(urlpath, "THUMB MEDIUM", '/app/images/imageout-MEDIUM.png', options, res);
 });
 
 app.get('/images/thumb/small/:url', (req, res) => { //first
   var urlpath = 'http://'+req.params.url;
-  wkhtmltoimage.generate(urlpath, { output: '/app/images/imageout.png' });
-  resizeImg(fs.readFileSync('/app/images/imageout.png'), {width: 512, height: 224}).then(buf => {
-    fs.writeFileSync('/app/images/imageout-SMALL.png', buf);
-    console.log("THUMB SMALL");
-    console.log("Image generated in folder: imageout-SMALL.png")
-	res.sendFile('/app/images/imageout-SMALL.png')});
+  var options = {
+  screenSize: {
+    width: 512
+  , height: 224
+  }
+, shotSize: {
+    width: 'all'
+  , height: 'all'
+  }
+  }
+  LeavesStaticClass.leavesImages(urlpath, "THUMB SMALL", '/app/images/imageout-SMALL.png', options, res);
 });
 
-var pagerank = require('google-pagerank');
-var rankerpage = 0;
-
-app.get('/meta/pagerank/:url', (req, res) => { //first
+app.get('/meta/pagerank/:url', (req, res) => {
   var urlpath = 'http://'+req.params.url;
-  pagerank(urlpath, function(err, rank) {
-  console.log("PAGE RANK");
-  console.log('Got pagerank', rank);
-  rankerpage = rank;
-  res.status(200).send({
-    rank: rankerpage,
-  });
-  });
-  
+  LeavesStaticClass.leavesPageRank(urlpath, "PAGE RANK", res);
+});
+
+app.get('/content/read/:url', (req, res) => {
+  var urlpath = 'http://'+req.params.url;
+  LeavesStaticClass.leavesRead(urlpath, "READABLE HTML", res);
 });
 
 //Note: Embed.ly account costs money
