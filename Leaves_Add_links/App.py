6b4ca6a9f50f229f56e744fa58b72339e4a392dd
Jagannath Bilgi <jagannath.bilgi@anant.us>
2019-02-20 20:40:28 +0530

    Included GetTagCount code to get (REST) tag and corresponding count

--- Leaves_Add_links/App.py
+++ Leaves_Add_links/App.py
@@ -11,12 +11,11 @@ example
 http://192.168.99.100:5500/params?url=https://developer.ibm.com/code/open/centers/codait/&tags=AI Fairness 360,IBM Code Model Asset Exchange&title=Test
 
 """
-
 import os
 from flask import Flask, request, jsonify
 import SubmitToQueue
 import json
-
+import GetTagCount
 try:
     leaves_port = os.environ["LEAVES_API_PORT"]
 except:
@@ -45,6 +44,10 @@ def params():
         return jsonify({"Status": "Failed - URL is missing"})
     SubmitToQueue.publishToRedis(title, url, tags)
     return jsonify({"Status":"Completed"})
+
+@app.route('/gettagcount')
+def getTagCount():
+    return (GetTagCount.getTagCount())
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=leaves_port)
---
+++ Leaves_Add_links/GetTagCount.py
@@ -0,0 +1,22 @@
+import mysql.connector as mariadb
+import json
+import os
+
+mysqluser = os.environ["DATABASE_USER"]
+mysqlpassword = os.environ["DATABASE_PASSWORD"]
+mysqldatabase = os.environ["DATABASE_NAME"]
+mysqlhost_db = os.environ["DATABASE_HOST"]
+
+def getTagCount():
+    mariadb_connection = mariadb.connect(user=mysqluser, password=mysqlpassword, database=mysqldatabase,host=mysqlhost_db)
+    cursor = mariadb_connection.cursor()
+    cursor.execute("select wt.id as id, wt.label as label, wt.slug as slug, count(1) as no_of_links from wallabag_entry_tag wet, wallabag_tag wt where wt.id = wet.tag_id group by wt.label")
+    row_headers = [x[0] for x in cursor.description]  # this will extract row headers
+    rv = cursor.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    return json.dumps(json_data)
+
+# if __name__ == '__main__':
+#     print(getTagCount())
