97e950e8b4714be2a4e1ce7a919ad79a8d8e697d
jsbilgi <jsbilgi@yahoo.com>
2020-04-10 19:06:32 +0530

    Modified dockerfile - changed mtsql-connector. Modified python script to accept port

--- Leaves_Add_links/GetTagCount.py
+++ Leaves_Add_links/GetTagCount.py
@@ -6,11 +6,12 @@ mysqluser = os.environ["DATABASE_USER"]
 mysqlpassword = os.environ["DATABASE_PASSWORD"]
 mysqldatabase = os.environ["DATABASE_NAME"]
 mysqlhost_db = os.environ["DATABASE_HOST"]
+mysqlport = os.environ["DATABASE_PORT"]
 
 def getTagCount():
-    mariadb_connection = mariadb.connect(user=mysqluser, password=mysqlpassword, database=mysqldatabase,host=mysqlhost_db)
+    mariadb_connection = mariadb.connect(user=mysqluser, password=mysqlpassword, database=mysqldatabase,host=mysqlhost_db,port=mysqlport)
     cursor = mariadb_connection.cursor()
-    cursor.execute("select wt.id as id, wt.label as label, wt.slug as slug, count(1) as no_of_links from wallabag_entry_tag wet, wallabag_tag wt where wt.id = wet.tag_id group by wt.label")
+    cursor.execute("select wt.id as id, wt.label as label, wt.slug as slug, count(1) as no_of_links from wallabag_entry_tag wet, wallabag_tag wt where wt.id = wet.tag_id group by wt.label, wt.slug, wt.id")
     row_headers = [x[0] for x in cursor.description]  # this will extract row headers
     rv = cursor.fetchall()
     json_data = []
